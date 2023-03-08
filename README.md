d52570576afcf548d3d7b0c7a19234bc191f28b8
Nanthachai <thecoachingonline@gmail.com>
2023-03-08 13:41:14 +0700

    my first

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hello-assembly
 Hello World Assembly
+
+nasm -f elf64 -o hello.o hello.asm
+
+ld -s -o hello hello.o
+
+(bash) ./hello
---
+++ hello.asm
@@ -0,0 +1,16 @@
+section .data
+    hello db 'Hello, World!',0
+section .text
+    global _start
+_start:
+    ; write hello message to stdout
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, hello
+    mov edx, 13
+    int 0x80
+
+    ; exit program
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
